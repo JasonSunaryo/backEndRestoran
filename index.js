63a0320b639eb3279b9c7f63d0f0cbd92e87fcde
JasonSunaryo <jasonsunaryo01@gmail.com>
2024-04-01 14:47:32 +0700

    Membuat login authentication

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3001;
+const port = 3012;
 const expressLayouts = require('express-ejs-layouts');
 
 app.set('view engine', 'ejs');
@@ -11,10 +11,29 @@ app.use(express.static('public'));
 
 
 app.get('/', (req,res) => {
-    res.render('index.ejs', {title:'Document',layout : 'MainLayout.ejs'});
+    res.render('main.ejs', {title:'Document',layout : 'MainLayout.ejs'});
 })
 
 app.listen(port, () => {
     console.log(`server running at http://localhost:${port}`);
 })
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     port: 3003
+//   }
+// })
+
+
+
+
+
+
+
+
 
