7afef8ed12fdba0d326bb33c63374608b889319e
Swiper0 <hansonsandy0406@gmail.com>
2024-04-01 21:29:30 +0700

    menggabungkan login dan crud menu

---
+++ server/routes/authRoute.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const authController = require('../controllers/authController');
+
+const router = express.Router();
+
+router.post('/signup', authController.signup);
+router.post('/login', authController.login);
+
+const User = require('../models/userModel');
+
+// Endpoint untuk mengambil data nama dari MongoDB
+router.get('/nama', async (req, res) => {
+    try {
+        const users = await User.find();
+        const nama = users.map(user => user.name);
+        res.json(nama);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+
+module.exports = router;
+    
